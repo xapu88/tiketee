3f38bf7a356ce07733fd6ad47071aacad1385611
xapu88 <hkalanj@gmail.com>
2016-02-04 02:34:00 +0100

    Impemented sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
     resources :tickets
   end
 
+  resources :users
   # Example resource route with options:
   #   resources :products do
   #     member do
